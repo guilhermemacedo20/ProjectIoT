d95f454d2a3660eb190f345aa5eccbb10d0687f0
Guilherme Macedo <guilhermeap.macedo20@gmail.com>
2026-03-31 18:39:02 -0300

    fix: adjust routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Routes, Route, HashRouter } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Estufa from "./pages/Estufa";
 
+
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/estufa/:id" element={<Estufa />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
