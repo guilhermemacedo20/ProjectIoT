18b2f9ea6e0a7c94331a09ebd35da47efa4cefad
Guilherme Macedo <guilhermeap.macedo20@gmail.com>
2026-03-31 00:57:45 -0300

    feat: add login layout

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,38 @@
 import { useNavigate } from "react-router-dom";
+import bgLogin from "../assets/backgroundLogin.png";
 
 export default function Login() {
   const navigate = useNavigate();
 
   return (
-    <div className="h-screen flex items-center justify-center bg-green-600">
-      <div className="bg-white p-6 rounded-xl shadow flex flex-col">
-        <h1 className="text-3xl font-bold mb-4 uppercase text-center bg-gradient-to-b from-green-900 via-green-700 to-green-500 bg-clip-text text-transparent">Monitoramento de Estufas</h1>
-        <input className="input mb-2" placeholder="Usuário" />
-        <input className="input mb-4" type="password" placeholder="Senha" />
+    <div
+      className="h-screen flex items-center justify-center"
+      style={{ backgroundImage: `url(${bgLogin})`, backgroundSize: "cover" }}
+    >
+      <div className="bg-white px-10 py-12 rounded-[40px] shadow-2xl flex flex-col w-[420px]">
+        
+        <h1 className="text-3xl font-bold mb-8 uppercase text-center bg-gradient-to-b from-green-900 via-green-700 to-green-500 bg-clip-text text-transparent">
+          Monitoramento de Estufas
+        </h1>
+
+        <input
+          className="mb-4 h-14 px-6 rounded-full bg-green-900 text-white placeholder-gray-300 focus:outline-none"
+          placeholder="Usuário"
+        />
+
+        <input
+          type="password"
+          className="mb-6 h-14 px-6 rounded-full bg-green-900 text-white placeholder-gray-300 focus:outline-none"
+          placeholder="Senha"
+        />
 
         <button
           onClick={() => navigate("/home")}
-          className="bg-green-600 text-white px-4 py-2 rounded align-center m-auto"
+          className="bg-green-600 hover:bg-green-700 transition text-white py-3 rounded-full font-semibold"
         >
           Entrar
         </button>
+
       </div>
     </div>
   );
